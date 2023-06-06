1467d5968db8af40734a7d8dabba2e9e831b3b35
rgao002 <rgao002@ucr.edu>
2023-06-06 16:38:23 -0700

    Added reducers and basic redux components

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,8 +1,10 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useReducer } from "react";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
+
+import { createAction } from "../utils/reducer/reducer.utils";
 /* Creating the Context: createContext() creates a
 new Context object. When React renders a component that
 subscribes to this Context object, it will read the current
@@ -15,13 +17,39 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+  console.log("dispatched payload", payload);
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
 /* Define a UserProvider component. An alias component that will 
 use the UserContext.Provider to provide the current user 
 and a function to set the current user to all components 
 within it (i.e., its children). */
 
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  // const { currentUser } = state;
+  const setCurrentUser = (user) => {
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+    // dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
   const value = { currentUser, setCurrentUser }; //values that we want to share
 
   useEffect(() => {
