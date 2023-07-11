f3fb47735f8ce55c72fff8c383ac8e257cb40085
rgao002 <rgao002@ucr.edu>
2023-07-11 13:33:02 -0700

    End of all

---
+++ script2.test.js
@@ -0,0 +1,39 @@
+const fetch = require("node-fetch");
+// import fetch from "node-fetch";
+const swapi = require("./script2");
+// import swapi from "./script2";
+
+it("calls swapi to get people", (done) => {
+  expect.assertions(1);
+  swapi.getPeople(fetch).then((data) => {
+    expect(data.count).toEqual(82);
+    done();
+  });
+});
+it("calls swapi to get people with a promise", () => {
+  expect.assertions(2);
+  return swapi.getPeoplePromise(fetch).then((data) => {
+    expect(data.count).toEqual(82);
+    expect(data.results.length).toBeGreaterThan(5);
+    // done();
+  });
+});
+
+it("getPeople returns count and results", () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 82,
+          results: [0, 1, 2, 3, 4, 5],
+        }),
+    })
+  );
+  expect.assertions(4);
+  return swapi.getPeoplePromise(mockFetch).then((data) => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith("https://swapi.dev/api/people");
+    expect(data.count).toEqual(82);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
